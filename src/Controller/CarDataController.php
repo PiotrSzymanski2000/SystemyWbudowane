99ef5c49a23a43aca24f0247d99dfd2cc8d14cd2
PiotrSzymanski2000 <piotreksz-18@wp.pl>
2022-12-06 14:58:38 +0100

    refactor styles and displaying a table

--- src/Controller/CarDataController.php
+++ src/Controller/CarDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CarDataRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class CarDataController extends AbstractController
 {
     #[Route('/car/data', name: 'app_car_data')]
-    public function index(): Response
+    public function index(CarDataRepository $carDataRepository): Response
     {
         return $this->render('car_data/index.html.twig', [
-            'controller_name' => 'CarDataController',
+            'carData' => $carDataRepository->findAll(),
         ]);
     }
 }
